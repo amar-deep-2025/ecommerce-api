e094813edb57be498e0236b506640d6dcef8ed34
varunmauraya2022 <varunmauraya@2022email.com>
2025-12-17 23:29:32 +0530

    add changes in UserController and use Dtos

--- src/main/java/com/amar/fullstack/ecommerce_api/controller/UserController.java
+++ src/main/java/com/amar/fullstack/ecommerce_api/controller/UserController.java
@@ -1,9 +1,13 @@
 package com.amar.fullstack.ecommerce_api.controller;
 
 
+import com.amar.fullstack.ecommerce_api.dto.UserRequestDto;
+import com.amar.fullstack.ecommerce_api.dto.UserResponseDto;
 import com.amar.fullstack.ecommerce_api.entities.User;
 import com.amar.fullstack.ecommerce_api.services.UserService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -14,14 +18,16 @@ public class UserController {
     @Autowired
     private UserService userService;
 
+
+
     @GetMapping
-    public List<User> getAll(){
+    public List<UserResponseDto> getAll(){
         return userService.getAllUsers();
     }
 
     @PostMapping
-    public User create(@RequestBody User user){
-        return userService.save(user);
+    public UserResponseDto create(@Valid @RequestBody UserRequestDto dto){
+        return userService.save(dto);
     }
 
 
