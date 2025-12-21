a730f42d3c5d5afeb57b52118161ee93cfb22fba
varunmauraya2022 <varunmauraya@2022email.com>
2025-12-21 13:34:20 +0530

    refactor JwtAuthenticationFilter  means add authorities_list

--- src/main/java/com/amar/fullstack/ecommerce_api/security/JwtAuthenticationFilter.java
+++ src/main/java/com/amar/fullstack/ecommerce_api/security/JwtAuthenticationFilter.java
@@ -7,12 +7,15 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
+import java.util.List;
 
 @Component
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
@@ -37,9 +40,15 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
                 .verify(token)
                 .getSubject();
 
+        String role=JWT.require(Algorithm.HMAC256(SECRET))
+                .build()
+                .verify(token)
+                .getClaim("role").asString();
+
         if (email!=null && SecurityContextHolder.getContext().getAuthentication()==null){
+           List<GrantedAuthority> authorityList= List.of(new SimpleGrantedAuthority("ROLE_"+role));
             UsernamePasswordAuthenticationToken authToken=
-                    new UsernamePasswordAuthenticationToken(email,null,null);
+                    new UsernamePasswordAuthenticationToken(email,null,authorityList);
 
             authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(req));
             SecurityContextHolder.getContext().setAuthentication(authToken);
