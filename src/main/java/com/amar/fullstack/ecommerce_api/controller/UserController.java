5180726ead76ef267380ef96536448b92600b881
varunmauraya2022 <varunmauraya@2022email.com>
2025-12-21 14:09:45 +0530

    refactor role based and pring role, name

--- src/main/java/com/amar/fullstack/ecommerce_api/controller/UserController.java
+++ src/main/java/com/amar/fullstack/ecommerce_api/controller/UserController.java
@@ -8,6 +8,8 @@ import com.amar.fullstack.ecommerce_api.services.UserService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -22,12 +24,20 @@ public class UserController {
 
     @GetMapping
     public List<UserResponseDto> getAll(){
+        //This
+        Authentication auth= SecurityContextHolder.getContext().getAuthentication();
+        System.out.println("USER: " + auth.getName());
+        System.out.println("ROLES: " + auth.getAuthorities());
         return userService.getAllUsers();
     }
 
     @PostMapping
     public UserResponseDto create(@Valid @RequestBody UserRequestDto dto){
-        System.out.println(dto.getName());
+        Authentication auth= SecurityContextHolder.getContext().getAuthentication();
+        //This log is for testing purpose to see which user is creating new user
+        System.out.println("USER: " + auth.getName());
+        System.out.println("ROLES: " + auth.getAuthorities());
+        System.out.println("Insert New User "+dto.getName());
         return userService.save(dto);
     }
 
