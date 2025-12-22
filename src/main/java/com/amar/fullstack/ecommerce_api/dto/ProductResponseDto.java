33b003e010ea21b9339e770d413926c96a76593d
varunmauraya2022 <varunmauraya@2022email.com>
2025-12-22 18:26:36 +0530

    add ProductResponseDto

---
+++ src/main/java/com/amar/fullstack/ecommerce_api/dto/ProductResponseDto.java
@@ -0,0 +1,50 @@
+package com.amar.fullstack.ecommerce_api.dto;
+
+public class ProductResponseDto {
+
+    private Long id;
+    private String name;
+    private Double price;
+    private String description;
+    private Double rating;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Double getRating() {
+        return rating;
+    }
+
+    public void setRating(Double rating) {
+        this.rating = rating;
+    }
+}
