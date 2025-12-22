f3a725fccca22b3ade256549c6e48e525b0423ca
varunmauraya2022 <varunmauraya@2022email.com>
2025-12-22 18:11:39 +0530

    add Product entity

---
+++ src/main/java/com/amar/fullstack/ecommerce_api/entities/Product.java
@@ -0,0 +1,76 @@
+package com.amar.fullstack.ecommerce_api.entities;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Table(name="products")
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private Double price;
+
+    @Column(length = 1000)
+    private String description;
+
+    @Column(nullable = false)
+    @Size(max=5)
+    private Double rating=0.0;
+
+    public Product(){}
+
+    public Product(Long id, String name, Double price, String description, Double rating) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.description = description;
+        this.rating = rating;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Double getRating() {
+        return rating;
+    }
+
+    public void setRating(Double rating) {
+        this.rating = rating;
+    }
+}
