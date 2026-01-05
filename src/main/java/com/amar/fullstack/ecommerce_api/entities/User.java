317c3e12a3d5a0eb07ef43d33a4f9cf7642b20f7
varunmauraya2022 <varunmauraya@2022email.com>
2026-01-05 17:34:24 +0530

    refactore User.java

--- src/main/java/com/amar/fullstack/ecommerce_api/entities/User.java
+++ src/main/java/com/amar/fullstack/ecommerce_api/entities/User.java
@@ -1,5 +1,6 @@
 package com.amar.fullstack.ecommerce_api.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 
 @Entity
@@ -16,6 +17,7 @@ public class User {
     @Column(nullable = false,unique = true)
     private String email;
 
+    @JsonIgnore
     private String password;
 
     @Enumerated(EnumType.STRING)
