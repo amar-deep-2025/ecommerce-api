8be8ec70dfa198d14a828ca52d6723dcba05bf56
varunmauraya2022 <varunmauraya@2022email.com>
2026-01-06 19:19:42 +0530

    add createPaymentRequestDto

--- src/main/java/com/amar/fullstack/ecommerce_api/dto/Payment/CreatePaymentRequestDto.java
+++ src/main/java/com/amar/fullstack/ecommerce_api/dto/Payment/CreatePaymentRequestDto.java
@@ -1,6 +1,14 @@
-package com.amar.fullstack.ecommerce_api.dto;
+package com.amar.fullstack.ecommerce_api.dto.Payment;
 
 public class CreatePaymentRequestDto {
 
     private Long ordrerId;
+
+    public Long getOrdrerId() {
+        return ordrerId;
+    }
+
+    public void setOrdrerId(Long ordrerId) {
+        this.ordrerId = ordrerId;
+    }
 }
