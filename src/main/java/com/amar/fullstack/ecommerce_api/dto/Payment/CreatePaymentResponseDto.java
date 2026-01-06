c8e15947348c8b1d485ad13961a2d97b2489ac09
varunmauraya2022 <varunmauraya@2022email.com>
2026-01-06 22:28:43 +0530

    add createPaymentResponseDto

--- src/main/java/com/amar/fullstack/ecommerce_api/dto/Payment/CreatePaymentResponseDto.java
+++ src/main/java/com/amar/fullstack/ecommerce_api/dto/Payment/CreatePaymentResponseDto.java
@@ -1,9 +1,43 @@
-package com.amar.fullstack.ecommerce_api.dto;
+package com.amar.fullstack.ecommerce_api.dto.Payment;
 
 public class CreatePaymentResponseDto {
 
     private Long paymentId;
     private Double totalAmount;
     private String gatewayUrl;
+
+    public String getRazorpayOrderId() {
+        return razorpayOrderId;
+    }
+
+    public void setRazorpayOrderId(String razorpayOrderId) {
+        this.razorpayOrderId = razorpayOrderId;
+    }
+
+    public String getGatewayUrl() {
+        return gatewayUrl;
+    }
+
+    public void setGatewayUrl(String gatewayUrl) {
+        this.gatewayUrl = gatewayUrl;
+    }
+
+    public Double getTotalAmount() {
+        return totalAmount;
+    }
+
+    public void setTotalAmount(Double totalAmount) {
+        this.totalAmount = totalAmount;
+    }
+
+    public Long getPaymentId() {
+        return paymentId;
+    }
+
+    public void setPaymentId(Long paymentId) {
+        this.paymentId = paymentId;
+    }
+
     private String razorpayOrderId;
+
 }
